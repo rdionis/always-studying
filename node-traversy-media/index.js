8be1a6d6b00a60391b1abeaaf08e5f875dbf1c99
Raquel Dionísio <dionisio.raquel@gmail.com>
2022-05-10 23:02:13 +0200

    refreshing node.js with brad traversy

---
+++ node-traversy-media/index.js
@@ -0,0 +1,7 @@
+const Person = require("./person"); // common JS
+
+const person1 = new Person("John Doe", 30);
+const person2 = new Person("Matilda Kott", 12);
+
+person1.greeting();
+person2.greeting();
---
+++ node-traversy-media/reference/path_demo.js
@@ -0,0 +1,2 @@
+const path = require("path");
+// 'path' is a core module, so you don't need the file path './' or to npm install anything
