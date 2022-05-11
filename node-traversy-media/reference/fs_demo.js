7d93d5531366da868b41d3096b856fe3c229e06c
Raquel Dionísio <dionisio.raquel@gmail.com>
2022-05-11 11:45:42 +0200

    operating system module in node

--- node-traversy-media/reference/fs_demo.js
+++ node-traversy-media/reference/fs_demo.js
@@ -1,23 +1,35 @@
 const fs = require("fs"); // fs = file system module
 const path = require("path");
 
-//Create folder
+// ****** CREATE FOLDER ******
 
 // fs.mkdir(path.join(__dirname, "/test"), {}, (err) => {
 //   if (err) throw err;
 //   console.log("Folder created...");
 // });
 
-// Create and write to file
-fs.writeFile(path.join(__dirname, "/test", "hello.txt"), "Hello, World!", (err) => {
+// ****** CREATE AND WRITE TO FOLDER ******
+// fs.writeFile(path.join(__dirname, "/test", "hello.txt"), "Hello, World!", (err) => {
+//   if (err) throw err;
+//   console.log("File written to...");
+
+// ****** FILE APPEND ******
+// we are running this inside the callback
+//   fs.appendFile(path.join(__dirname, "./test", "hello.txt"), " I love Node.js", (err) => {
+//     if (err) throw err;
+//     console.log("File written to again...");
+//   });
+// });
+
+// ****** READ FILE ******
+// fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf8", (err, data) => {
+//   // "utf8" here is so that we'll be shown the proper text instead of something like: "<Buffer 48 65 6c 6c 6f 2c 20 57 6f 72 6c 64 21 20 49 20 6c 6f 76 65 20 4e 6f 64 65 2e 6a 73>"
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// ****** RENAME FILE ******
+fs.rename(path.join(__dirname, "/test", "hello.txt"), path.join(__dirname, "/test", "helloworld.txt"), (err) => {
   if (err) throw err;
-  console.log("File written to...");
-
-  //File append
-  fs.appendFile(path.join(__dirname, "./test", "hello.txt"), " I love Node.js", (err) => {
-    if (err) throw err;
-    console.log("File written to again...");
-  });
+  console.log("File renamed...");
 });
-
-// MINUTO 35:24
---
+++ node-traversy-media/reference/os_demo.js
@@ -0,0 +1,25 @@
+// ****** OPERATING SYSTEM ******
+
+const os = require("os");
+const { CLIENT_RENEG_LIMIT } = require("tls");
+
+// ****** PLATFORM ******
+console.log(os.platform()); // returns linux or whatever the platform your OS works on
+
+// ****** CPU ARCH ******
+console.log(os.arch()); // x64
+
+// ****** CPU CORE INFO ******
+console.log(os.cpus()); // returns an object with info for every core of the CPU
+
+// ****** FREE MEMORY ******
+console.log(os.freemem());
+
+// ****** TOTAL MEMORY ******
+console.log(os.totalmem());
+
+// ****** HOME DIRECTORY ******
+console.log(os.homedir());
+
+// ****** UPTIME ******
+console.log(os.uptime());
